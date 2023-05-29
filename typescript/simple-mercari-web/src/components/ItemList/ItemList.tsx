b7341987418320927f452fc2e64099e4cc274e9e
Airi-hub <dreamair25@gmail.com>
2023-05-29 15:34:50 +0900

    step7、各アイテムの画像を表示、スタイル変更、 アイテム一覧のUIを変更

--- typescript/simple-mercari-web/src/components/ItemList/ItemList.tsx
+++ typescript/simple-mercari-web/src/components/ItemList/ItemList.tsx
@@ -5,61 +5,72 @@ interface Item {
   name: string;
   category: string;
   image_filename: string;
-};
+}
 
 const server = process.env.REACT_APP_API_URL || 'http://127.0.0.1:9000';
-const placeholderImage = process.env.PUBLIC_URL + '/logo192.png';
+const placeholderImage = process.env.PUBLIC_URL + '/default.jpg';
 
-interface Prop {
+interface Props {
   reload?: boolean;
   onLoadCompleted?: () => void;
 }
-
-export const ItemList: React.FC<Prop> = (props) => {
+// Propsの名前を修正
+export const ItemList: React.FC<Props> = (props) => {
   const { reload = true, onLoadCompleted } = props;
-  const [items, setItems] = useState<Item[]>([])
+  const [items, setItems] = useState<Item[]>([]);
+
   const fetchItems = () => {
-    fetch(server.concat('/items'),
-      {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        },
+    fetch(`${server}/items`, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    //レスポンスデータの取り扱いを修正
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('GET error: ' + response.status);
+        }
       })
-      .then(response => response.json())
       .then(data => {
         console.log('GET success:', data);
         setItems(data.items);
         onLoadCompleted && onLoadCompleted();
       })
       .catch(error => {
-        console.error('GET error:', error)
-      })
-  }
+        console.error('GET error:', error);
+      });
+  };
 
   useEffect(() => {
     if (reload) {
       fetchItems();
     }
-  }, [reload]);
+  }, [reload, onLoadCompleted]);  //依存配列にonLoadCompletedを追加
 
   return (
-    <div>
-      {items.map((item) => {
-        return (
-          <div key={item.id} className='ItemList'>
-            {/* TODO: Task 1: Replace the placeholder image with the item image */}
-            <img src={placeholderImage} />
-            <p>
-              <span>Name: {item.name}</span>
-              <br />
-              <span>Category: {item.category}</span>
-            </p>
-          </div>
-        )
-      })}
+    <div className='ItemListing'>
+      {items.map((item) => (
+        <div key={item.id} className='ItemList'>
+          <img
+            src={`${server}/image/${item.image_filename}`}
+            alt={item.name}
+            onError={(e) => {
+              e.currentTarget.src = placeholderImage;
+            }}
+          />
+    
+          <p>
+            <span>Name: {item.name}</span>
+            <br />
+            <span>Category: {item.category}</span>
+          </p>
+        </div>
+      ))}
     </div>
-  )
+  );  
 };
